c065ad286c6df53cfda69109b76a0d816131e16b
estudart <erico.studartdf@gmail.com>
2024-02-05 09:29:13 -0300

    connecting to database

--- model/__init__.py
+++ model/__init__.py
@@ -1,1 +1,29 @@
 from base import Base
+from estadio import Estadio
+from jogador import Jogador
+from tecnico import Tecnico
+
+import sqlite3
+
+from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, event
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+import os
+
+# url de acesso ao banco (essa é uma url de acesso ao sqlite local)
+db_url = 'sqlite:///database/db.sqlite3'
+
+# cria a engine de conexão com o banco
+engine = create_engine(db_url, echo=False)
+
+# Instancia um criador de seção com o banco
+Session = sessionmaker(bind=engine)
+
+# cria o banco se ele não existir 
+if not database_exists(engine.url):
+    create_database(engine.url) 
+
+# cria as tabelas do banco, caso não existam
+Base.metadata.create_all(engine)
